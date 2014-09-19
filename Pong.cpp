52b3b0a302d3ec523f579acdbb3a7a9040263255
Kiwii12 <johnathan.ackerman@mines.sdsmt.edu>
2014-09-19 15:05:27 -0600

    Added basic drawing abilities and test code

--- Pong.cpp
+++ Pong.cpp
@@ -1,6 +1,10 @@
 #include "Pong.h"
 
+const float White[]     = { 1.0, 1.0, 1.0 };
+
 Pong* Pong::instance;
+
+const float ViewplaneSize = 1000.0;
 
 Pong::Pong()
 {
@@ -53,10 +57,52 @@ int Pong::run( int argc, char *argv[] )
 }
 
 void Pong::display()
-{ }
+{
+	//clear the display and set backround to black
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	glColor3fv( White );
+    glBegin( GL_LINES );
+        glVertex2f( 0, -1000 );
+        glVertex2f( 0, 1000 );
+    glEnd();
+
+	glColor3fv( White );
+    glBegin( GL_LINES );
+        glVertex2f( -1000, -1000 );
+        glVertex2f( 1000, -1000 );
+    glEnd();
+
+	glColor3fv( White );
+    glBegin( GL_LINES );
+        glVertex2f( -1000, 999 );
+        glVertex2f( 1000, 999 );
+    glEnd();
+
+	// flush graphical output
+    glutSwapBuffers();
+    glFlush();
+}
 
 void Pong::reshape(int w, int h)
-{ }
+{
+	// store new window dimensions globally
+    wWidth = w;
+    wHeight = h;
+
+    // orthographic projection of 3-D scene onto 2-D, maintaining aspect ratio
+    glMatrixMode( GL_PROJECTION );      // use an orthographic projection
+    glLoadIdentity();                   // initialize transformation matrix
+    if ( w > h )                        // use width:height aspect ratio to specify view extents
+        gluOrtho2D( -ViewplaneSize * w / h, ViewplaneSize * w / h, -ViewplaneSize, ViewplaneSize );
+    else
+        gluOrtho2D( -ViewplaneSize, ViewplaneSize, -ViewplaneSize * h / w, ViewplaneSize * h / w );
+    glViewport( 0, 0, w, h );           // adjust viewport to new window
+
+    // switch back to (default) model view mode, for transformations
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+}
 
 void Pong::keyboard(unsigned char key, int x, int y)
 { }
