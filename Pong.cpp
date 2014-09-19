617d057a22f8c6e9ec9f064a02e0bb0136c74627
Deaboy <danieleandrus@gmail.com>
2014-09-19 14:07:43 -0600

    Added event handlers
    Note: will not compile because of LNK2001 error

--- Pong.cpp
+++ Pong.cpp
@@ -3,10 +3,17 @@
 void initOpenGL();
 
 Pong::Pong()
-{ }
+{
+	instance = this;
+}
 
 Pong::~Pong()
 { }
+
+Pong* Pong::getInstance()
+{
+	return instance;
+}
 
 int Pong::run( int argc, char *argv[] )
 {
@@ -16,18 +23,18 @@ int Pong::run( int argc, char *argv[] )
 
     glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );     // 32-bit graphics and single buffering
 
-    glutInitWindowSize( ScreenWidth, ScreenHeight );    // initial window size
-    glutInitWindowPosition( 100, 50 );                  // initial window position
-    glutCreateWindow( "OpenGL Demo" );                  // window title
+    glutInitWindowSize( this -> wWidth, this -> wHeight);    // initial window size
+    glutInitWindowPosition( this -> wLeft, this -> wHeight );                  // initial window position
+    glutCreateWindow( this -> wName.c_str() );                  // window title
 
     glClearColor( 0.0, 0.0, 0.0, 1.0 );                 // use black for glClear command
 
     // callback routines
-    glutDisplayFunc( display );                         // how to redisplay window
-    glutReshapeFunc( reshape );                         // how to resize window
-    glutKeyboardFunc( keyboard );                       // how to handle key presses
-    glutMouseFunc( mouseclick );                        // how to handle mouse events
-    glutIdleFunc( idle );                               // animation callback
+    glutDisplayFunc( *::display );                         // how to redisplay window
+    glutReshapeFunc( *::reshape );                         // how to resize window
+    glutKeyboardFunc( *::keyboard );                       // how to handle key presses
+    glutMouseFunc( *::mouseclick );                        // how to handle mouse events
+    glutIdleFunc( *::idle );                               // animation callback
 
 
     // go into OpenGL/GLUT main loop, never to return
@@ -36,4 +43,46 @@ int Pong::run( int argc, char *argv[] )
     // yeah I know, but it keeps compilers from bitching
     return 0;
 
+}
+
+void Pong::display()
+{ }
+
+void Pong::reshape(int w, int h)
+{ }
+
+void Pong::keyboard(unsigned char key, int x, int y)
+{ }
+
+void Pong::mouseclick(int button, int state, int x, int y)
+{ }
+
+void Pong::idle()
+{ }
+
+////////////////////////////////////////////////////////////////////////////////
+
+void display()
+{
+	Pong::getInstance()->display();
+}
+
+void reshape(int w, int h)
+{
+	Pong::getInstance()->reshape(w, h);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	Pong::getInstance()->keyboard(key, x, y);
+}
+
+void mouseclick(int button, int state, int x, int y)
+{
+	Pong::getInstance()->mouseclick(button, state, x, y);
+}
+
+void idle()
+{
+	Pong::getInstance()->idle();
 }
--- Pong.h
+++ Pong.h
@@ -2,20 +2,40 @@
 #define _PONG_H_
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 #include <gl/freeglut.h>
 
 class Pong
 {
+	private:
+		static Pong* instance;
+		int wWidth = 400;
+		int wHeight = 300;
+		int wTop = 50;
+		int wLeft = 100;
+		string wName = "Pong";
+
 	public:
 		Pong ( );
-
 		~Pong ( void );
-
-		int run ( );
+		static Pong* getInstance( );
+		int run ( int argc, char *argv[] );
+		
+		void display();
+		void reshape( int w, int h );
+		void keyboard( unsigned char key, int x, int y );
+		void mouseclick( int button, int state, int x, int y );
+		void idle();
 
 
 };
+
+void display();
+void reshape( int w, int h );
+void keyboard( unsigned char key, int x, int y );
+void mouseclick( int button, int state, int x, int y );
+void idle();
 
 #endif
