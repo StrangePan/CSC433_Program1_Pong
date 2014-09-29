165317f6424e861e931fc0499c8dbf8e3e5b1ba0
Kiwii12 <johnathan.ackerman@mines.sdsmt.edu>
2014-09-29 16:59:12 -0600

    Doxygen for Ball.h
    still very brief, no sign of par description yet

--- Ball.h
+++ Ball.h
@@ -8,18 +8,39 @@ class Ball;
 #include "Steppable.h"
 #include "PongGame.h"
 
+/*! 
+ * @brief The Ball Class Holds all the information to draw a ball for Pong
+ *
+ * @details The Ball holds information for its position, speed, and size
+ *
+ */
 class Ball : public Drawable, public Steppable
 {
 	public:
-		double center_x;
-		double center_y;
-		int width;
-		double x_velocity;
-		double y_velocity;
-		PongGame* game;
+		double center_x;	/*!< x coordinate for the center of the ball on
+									in the coordinate plane */
+		double center_y;	/*!< y coordinate for the center of the ball on
+									in the coordinate plane */
+		int width;			/*!< radius of the ball */
+		double x_velocity;	/*!< x speed of the ball */
+		double y_velocity;	/*!< y speed of the ball */
+		PongGame* game;		/*!< instance of the pongGame class that is in
+									charge of the ball class */
 
+		/*!
+		 *  @brief The constructor, creates the ball with location, size, and
+		 *			speed
+		 */
 		Ball(PongGame* game, double x, double y, int width, double x_velocity, double y_velocity);
+
+		/*!
+		 *  @brief Handles the ball movement
+		 */
 		void step();
+
+		/*!
+		 *  @brief draws the ball with the drawable class
+		 */
 		void draw();
 };
 
