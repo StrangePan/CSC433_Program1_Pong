a3ad0259779c10d2765759a1b1fcea671b81e33c
Deaboy <danieleandrus@gmail.com>
2014-09-30 16:01:45 -0600

    Documentation
    Added documentation to the following files:
    - PaddleController.h
    - PlayerController.h

--- PaddleController.h
+++ PaddleController.h
@@ -1,18 +1,58 @@
+/***************************************************************************//**
+ * @file File containing the declaration for the PaddleController class.
+ *
+ * @brief Contains the declaration for the PaddleController class, an interface
+ *		for classes designed to control paddles.
+*******************************************************************************/
 #ifndef _PADDLECONTROLLER_H_
 #define _PADDLECONTROLLER_H_
 
+/*******************************************************************************
+ *                 DECLARATIONS, INCLUDES, AND NAMESPACES
+*******************************************************************************/
 class PaddleController;
 
 #include "Steppable.h"
 
+/***************************************************************************//**
+ * @brief The PaddleController class is really an interface for objects designed
+ *		to take control of a Paddle object.
+ *
+ * @details This class declares a set of pure virtual functions that all objects
+ *		designed to control a paddle must have in order to be valid.
+*******************************************************************************/
 class PaddleController : public Steppable
 {
 	public:
+
+		/*!
+		 * @brief Handles key press events.
+		 */
 		virtual void keyDown(unsigned char key) = 0;
+		
+		/*!
+		 * @brief Handles key release events.
+		 */
 		virtual void keyUp(unsigned char key) = 0;
+		
+		/*!
+		 * @brief Handles special key press events.
+		 */
 		virtual void keySpecialDown(int key) = 0;
+		
+		/*!
+		 * @brief Handles special key release events.
+		 */
 		virtual void keySpecialUp(int key) = 0;
+		
+		/*!
+		 * @brief Handles collision with ball events.
+		 */
 		virtual void ballHit() = 0;
+		
+		/*!
+		 * @brief Executes every game step.
+		 */
 		virtual void step() = 0;
 };
 
--- PlayerController.h
+++ PlayerController.h
@@ -1,26 +1,68 @@
+/***************************************************************************//**
+ * @file File containing the declaration for the PlayerController class.
+ *
+ * @brief Contains the declaration for the PlayerController class, a class
+ *		designed to allow a player to control a Pong paddle.
+*******************************************************************************/
 #ifndef _PLAYERCONTROLLER_H_
 #define _PLAYERCONTROLLER_H_
 
+/*******************************************************************************
+ *                 DECLARATIONS, INCLUDES, AND NAMESPACES
+*******************************************************************************/
 class PlayerController;
 
 #include <GL/freeglut.h>
 #include "PaddleController.h"
 #include "Paddle.h"
 
+/***************************************************************************//**
+ * @brief The PlayerController class is designed to allow a user to control
+ *		a Pong paddle through 
+ *
+ * @details This class declares a set of pure virtual functions that all objects
+ *		designed to control a paddle must have in order to be valid.
+*******************************************************************************/
 class PlayerController : public PaddleController
 {
 	private:
-		bool arrows;
-		Paddle* paddle;
+		bool arrows;	/*<! Flag to check for arrows or letters */
+		Paddle* paddle;	/*<! Pointer to paddle we're controlling */
 
 	public:
+		/*!
+		 * @brief The constructor. Initializes variables.
+		 */
 		PlayerController(Paddle* paddle, bool arrows);
-
+		
+		/*!
+		 * @brief Handles WASD keys being pressed.
+		 */
 		void keyDown(unsigned char key);
+		
+		/*!
+		 * @brief Handles WASD keys being released.
+		 */
 		void keyUp(unsigned char key);
+		
+		/*!
+		 * @brief Handles arrow keys being pressed.
+		 */
 		void keySpecialDown(int key);
+		
+		/*!
+		 * @brief Handles arrow keys being released.
+		 */
 		void keySpecialUp(int key);
+		
+		/*!
+		 * @brief Does nothing; fulfills superclass requirements.
+		 */
 		void ballHit();
+		
+		/*!
+		 * @brief Does nothing; fulfills superclass requirements.
+		 */
 		void step();
 };
 
