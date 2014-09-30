2109110af33ddee18d9d7c0b52e16ef5ccb47d07
Kiwii12 <johnathan.ackerman@mines.sdsmt.edu>
2014-09-30 16:42:32 -0600

    Added draw functions
    + menu and items are drawable
    - functions are very ambigous

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,8 @@
 #include "Menu.h"
+
+void Menu::draw()
+{
+	glColor3d( 1.0, 0.8, 0.8 );
+	glRecti( x - (width / 2), y - (height / 2), x + (width / 2), y + ( height / 2 ) );
+    glRecti( x - border, y - border, x, y + height + border );
+}
--- MenuItem.cpp
+++ MenuItem.cpp
@@ -1,1 +1,9 @@
 #include "MenuItem.h"
+
+void MenuItem::draw()
+{
+	glColor3d( 1.0, 1.0, 1.0 );
+	glRecti( x - (width / 2), y - (height / 2), x + (width / 2), y + ( height / 2 ) );
+	glColor3d( 1.0, 1.0, 1.0 );
+    glRecti( x - (width / 2) + border, y - (height / 2) + border, x + (width / 2) - border, y + ( height / 2 ) - border );
+}
