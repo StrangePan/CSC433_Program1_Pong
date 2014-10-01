5d41c192b18c90f743d15ee53fe70b0444a91f09
Daniel Andrus <danieleandrus@gmail.com>
2014-09-30 20:49:27 -0600

    Added even MOAR stuff

--- MenuItem.h
+++ MenuItem.h
@@ -4,13 +4,16 @@
  * @brief Contains the declaration for the MenuItem class.
 *******************************************************************************/
 #ifndef _MENUITEM_H_
-#define _MENUTEM_H_
+#define _MENUITEM_H_
 
-class Menu;
+class MenuItem;
 
 #include <string>
+#include <functional>
 #include "Drawable.h"
 #include "PongText.h"
+#include "Pong.h"
+#include "Menu.h"
 
 using namespace std;
 
@@ -24,12 +27,17 @@ private:
 	double height;
 	double border;
 	PongText text;
+	function<void ()> callback;
 
 public:
+	MenuItem(Menu* menu, double x, double y, double width, double height, string text, function<void ()> callback = [](){});
+	
+	void setPosition( double x, double y );
+	void setSize( double width, double height );
+	void setText( string text );
+	void setAction( function<void ()> callback );
+	void click( int button, int state, int x, int y );
 	void draw();
-	void setPosition( double new_x, double new_y );
-	void setText( string new_text );
-	void getClicked( );
 };
 
 #endif
